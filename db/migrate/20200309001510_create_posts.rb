bbfabdbc942e93c3be682830baee1feb39ea13f5
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-03-10 21:08:26 +0000

    fixed typos

--- db/migrate/20200309001510_create_posts.rb
+++ db/migrate/20200309001510_create_posts.rb
@@ -1,8 +1,18 @@
-class CreatePosts < ActiveRecord::Migration[6.0]
+#class CreatePosts < ActiveRecord::Migration[6.0]
+#  def change
+#    create_table :posts do |t|
+#      t.string :title
+#      t.string :content
+#    end
+#  end
+#end
+
+
+class CreatePosts < ActiveRecord::Migration
   def change
-    create_table :posts |t|
-    t.string :title
-    t.text :text
-    t.timestamps
+    create_table :posts do |t|
+      t.string :title
+      t.string :content
+    end
   end
 end
